301cd72acbad3f794ef98f1655475cfdad2127a0
komool <67896210+komool@users.noreply.github.com>
2022-05-24 21:55:07 +0900

    바이오헬스 링크, 로고 이미지 추가

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const mainUrl = "https://wsu.ac.kr/";
 const infoUrl = "https://info.wsu.ac.kr/index_new.jsp";
 const lmsUrl = "https://smart.wsu.ac.kr/";
 const scheUrl = "https://wsu.ac.kr/page/index.jsp?code=campus0101";
+const bioUrl = "https://lms.biocoss.ac.kr/";
 
 document.getElementById("main").addEventListener("click", () => newTab(mainUrl), false);
 document.getElementById("info").addEventListener("click", () => newTab(infoUrl), false);
 document.getElementById("lms").addEventListener("click", () => newTab(lmsUrl), false);
 document.getElementById("sche").addEventListener("click", () => newTab(scheUrl), false);
+document.getElementById("bio").addEventListener("click", () => newTab(bioUrl), false);
 
 function newTab(url)
 {
